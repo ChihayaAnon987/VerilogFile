c9647a1da0d8ac5201656758902a10938ce7cb26
ChihayaAnon987 <2048324682@qq.com>
2024-11-04 14:14:53 +0800

    VerilogProgram

--- reg8file/reg8file.srcs/sim_1/new/testbench.v
+++ reg8file/reg8file.srcs/sim_1/new/testbench.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: ChihayaAnon987
 // 
 // Create Date: 2024/10/9 8:54:16
 // Design Name: 
