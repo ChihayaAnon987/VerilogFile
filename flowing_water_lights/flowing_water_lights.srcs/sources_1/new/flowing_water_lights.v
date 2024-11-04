84d2a8efb478c389879dbd9c67c22725bc9e146b
ChihayaAnon987 <2048324682@qq.com>
2024-11-04 16:37:56 +0800

    VerilogProgram

--- flowing_water_lights/flowing_water_lights.srcs/sources_1/new/flowing_water_lights.v
+++ flowing_water_lights/flowing_water_lights.srcs/sources_1/new/flowing_water_lights.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: ChihayaAnon987
+// Engineer: 
 // 
 // Create Date: 2024/10/16 14:13:32
 // Design Name: 
