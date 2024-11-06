f6d4dd8fdf07329ccb10aa43a96f2fb228dd2520
ChihayaAnon987 <2048324682@qq.com>
2024-11-06 21:05:02 +0800

    顶层模块

--- CompLab/CompLab.srcs/sources_1/new/CompLab.v
+++ CompLab/CompLab.srcs/sources_1/new/CompLab.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: ChihayaAnon987
 // 
 // Create Date: 2024/11/05 14:52:47
 // Design Name: 
